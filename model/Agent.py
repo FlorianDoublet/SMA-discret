63172d3294fef8bc8a05930976d83d5f10788e92
FlorianDoublet <florian.doublet@hotmail.fr>
2017-01-04 17:37:11 +0100

    [init] fill classes with their simple property

---
+++ model/Agent.py
@@ -0,0 +1,10 @@
+
+class Agent:
+
+    def __init__(self):
+        self.color
+        self.x
+        self.y
+
+    def decide(self):
+        pass
---
+++ model/Environnement.py
@@ -0,0 +1,9 @@
+
+class Environnement:
+
+    def __init__(self):
+        self.w
+        self.h
+        self.grille2d = [[0 for x in range(self.w)] for y in range(self.h)]
+
+
