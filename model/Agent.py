aa4447f1eb00e933066f65be82e1cab577b91dd1
FlorianDoublet <florian.doublet@hotmail.fr>
2017-01-07 18:48:21 +0100

    [feat] Implement the logic for Agent's move in the Environnement

--- model/Agent.py
+++ model/Agent.py
@@ -3,7 +3,8 @@ from random import randrange
 
 class Agent:
 
-    _directions = ["up", "down", "right", "left", "up-right", "up-left", "down-right", "down-left"]
+    _directions = [["up"], ["down"], ["right"], ["left"], ["up", "right"], ["up", "left"], ["down", "right"], ["down", "left"]]
+    _inverse_directions = { "up" : "down", "down" : "up", "left" : "right", "right" : "left" }
 
     def __init__(self, color, x, y, environnement):
         self.color = color
@@ -11,12 +12,109 @@ class Agent:
         self.y = y
         self.environnement = environnement
         # un agent s'ajoute lui meme dans l'environnement dans l'initialisation
-        self.environnement.grille2d[y][x] = self
+        self.environnement.set_agent(self)
+        self.previous_x = None
+        self.previous_y = None
         self.default_direction = self.random_direction()
 
+    def update(self):
+        """
+        met a jour la position de l'agent sur la carte
+        :return:
+        """
+        self.reset_old_position_in_env()
+        self.environnement.set_agent(self)
+
+
+
     def decide(self):
-        pass
+        """
+        decide quoi faire pour le mouvement de la particule (si elle se contente d'avancer ou non en cas de collision)
+        :return:
+        """
+
+        # la prochain position selon la direction
+        x, y = self.next_position()
+
+        collision = self.environnement.is_their_a_collision(x, y)
+
+        if collision:
+            # si il y a une collision
+            if type(collision) is Agent :
+                agent_col = collision
+                # Si la collision est un Agent, alors on inverse les directions des deux agents
+                # et on n'update pas ( on avance pas )
+                tmp_dir = agent_col.default_direction
+                agent_col.default_direction = self.default_direction
+                self.default_direction = tmp_dir
+                return
+
+            elif type(collision) is str or type(collision) is list :
+                # Si c'est une string ou une list alors c'est une collision avec un mur
+                self.wall_collision(collision)
+                return
+        else:
+            # their is no collision so we can update the position of our Agent
+            self.save_previous_pos
+            self.x = x
+            self.y = y
+            self.update()
+
+
+
+    def wall_collision(self, mur):
+        # on va dans l'axe oppose du mur rencontré
+        for direction in self.default_direction:
+            if direction in mur:
+                direction = self._inverse_directions[direction]
 
     def random_direction(self):
+        """
+        choisit une direction random parmis le voisinage de Moore
+        :return:
+        """
         direction_size = len(self._directions)
         return self._directions[randrange(direction_size)]
+
+    def next_position(self):
+        """
+        donne la prochaine position celon la direciton de la particule
+        :return: x, y qui correspondent a la position
+        """
+        dir = self.default_direction
+        y = self.y
+        x = self.x
+
+        if "up" in dir :
+            y -= 1
+        if "down" in dir :
+            y += 1
+        if "left" in dir:
+            x -= 1
+        if "right" in dir:
+            x += 1
+
+        if self.environnement.is_torrique:
+            y_max = self.environnement.h-1
+            x_max = self.environnement.w-1
+            # si on peux passer a travers les murs alors on calcul la position en fonction de ce parametre
+            if y < 0:
+                y = y_max
+            elif y > y_max:
+                y = 0
+
+            if x < 0:
+                x = x_max
+            elif x > x_max:
+                x = 0
+
+        return x, y
+
+    def save_previous_pos(self):
+        self.previous_x = self.x
+        self.previous_y = self.y
+
+    def reset_old_position_in_env(self):
+        self.environnement.delete_agent(self)
+
+
--- model/Environnement.py
+++ model/Environnement.py
@@ -8,6 +8,62 @@ class Environnement:
         self.SMA = SMA
         self.is_torrique = is_torrique
 
+    def set_agent(self, agent):
+        self.grille2d[agent.y][agent.x] = agent
+
+    def delete_agent(self, agent):
+        self.grille2d[agent.y][agent.x] = None
+
+
+    def is_their_a_collision(self, x, y):
+        """
+        Methode pour voir si il y a une quelquonque collision
+        :param agent:
+        :param x: prochaine position x de l'agent
+        :param y: prochaine position y de l'agent
+        :return: un agent percute, soit un mur traverse, soit rien
+        """
+        potential_agent = self.grille2d[y][x]
+        if potential_agent:
+            # si il y a collision avec un autre agent, on renvois cet agent
+            return potential_agent
+
+        if y < 0 or x < 0 or x > self.w-1 or y > self.h-1:
+            # si il y a collision avec un mur
+            return self.wall_collision_direction(x, y, self.w-1, self.h-1)
+
+    @staticmethod
+    def wall_collision_direction(x, y, max_x, max_y):
+        """
+
+        :param x: prochaine position x de l'agent
+        :param y: prochaine position y de l'agent
+        :param max_x: borne max de x
+        :param max_y: borne max de y
+        :return: la "position" ou "direction" (on peux le voir comme on veux) du mur percute ou traverse
+        """
+        if y < 0 and x < 0:
+            return ["up", "left"]
+        if y > max_y and x < 0:
+            return ["down", "left"]
+        if y < 0 and x > max_x:
+            return ["up", "right"]
+        if y > max_y and x > max_x:
+            return ["down", "right"]
+        if y < 0:
+            return "up"
+        if y > max_y:
+            return "down"
+        if x < 0:
+            return "right"
+        if x > max_x:
+            return "left"
+
+
+
+
+
+
 
 
 
