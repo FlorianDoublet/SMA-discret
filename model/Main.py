e7ad6bac84516495a4e736dbeef9d4d572edcf52
Quentin Delvallet <quentin.delvallet@hotmail.fr>
2017-01-09 22:03:32 +0100

    Added grid and box_sizes

--- model/Main.py
+++ model/Main.py
@@ -4,17 +4,23 @@ from model.SMA import SMA
 from vue.Vue import Vue
 from time import sleep
 
+
 class Main:
 
     def __init__(self):
-        sma = SMA(300, 300, 1)
-        vue = Vue(300, 300)
+        w = 20
+        h = 20
+        n = 5
+        box_size = 10
+
+        sma = SMA(w, h, n)
+        vue = Vue(w, h, box_size)
 
         sma.register(vue)
 
         while(True):
             sma.run()
-            sleep(0.02)
+            sleep(0.2)
 
         vue.end_draw()
 
--- vue/Vue.py
+++ vue/Vue.py
@@ -3,25 +3,33 @@ from observerPattern.observer import Observer
 
 
 class Vue(Observer):
-    def __init__(self, w, h):
+    def __init__(self, w, h, box_size):
         self.w = w
         self.h = h
+        self.box_size = box_size
         self.fen = Tk()
-        self.canvas = Canvas(self.fen, bg="darkblue", height=h, width=w)
+        self.canvas = Canvas(self.fen, bg="darkblue", height=h*self.box_size, width=w*self.box_size)
         self.canvas.pack()
         self.fen.update_idletasks()
         self.fen.update()
 
+    def draw_grid(self):
+        if self.box_size > 1:
+            for i in range(0, self.w * self.box_size, self.box_size):
+                self.canvas.create_line(0, i, self.h*self.box_size, i, fill='black')
+
+            for j in range(0, self.h * self.box_size, self.box_size):
+                self.canvas.create_line(j, 0, j, self.h*self.box_size, fill='black')
+
     def update(self, *args, **kwargs):
         environnement = args[0]
         self.canvas.delete("all")
+        self.draw_grid()
         agents = environnement.SMA.agent_list
 
         for agent in agents:
-            id = self.canvas.create_oval(-5, -5, 5, 5, fill=agent.color, outline='black')
-            id2 = self.canvas.create_oval(0, 0, 0, 0, fill='white', outline='')
-            self.canvas.move(id, agent.x, agent.y)
-            self.canvas.move(id2, agent.x, agent.y)
+            id = self.canvas.create_oval(0, 0, self.box_size, self.box_size, fill=agent.color, outline='black')
+            self.canvas.move(id, agent.x*self.box_size, agent.y*self.box_size)
 
         self.fen.update_idletasks()
         self.fen.update()
