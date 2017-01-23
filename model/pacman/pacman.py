dc0f85505f308527f93f9c832fdd6a6ea67216a9
Quentin Delvallet <quentin.delvallet@hotmail.fr>
2017-01-23 18:55:11 +0100

    Added random wall init

--- model/pacman/pacman.py
+++ model/pacman/pacman.py
@@ -28,11 +28,10 @@ class Pacman(Core):
         self.every_possible_tuple_position = [i for i in self.every_possible_tuple_position if i[index] < min(self.w, self.h)]
 
         # Recuperer x valeurs de toutes les permutations possible sans doublons
-        positions = random.sample(self.every_possible_tuple_position, 1)
 
         self.agent_list = []
-        agent_pos = positions[0]
-        self.agent_list.append(Avatar("yellow", agent_pos[0], agent_pos[1],self.environnement, self.is_trace))
+        self.init_walls()
+        self.init_avatar()
 
     def print_tick(self):
         pass
@@ -43,13 +42,46 @@ class Pacman(Core):
         if self.next_defender <= 0:
             self.create_new_defender()
 
+    def init_avatar(self):
+        agent_pos = random.sample(self.every_possible_tuple_position, 1)
+        agent_pos = agent_pos[0]
+        position = self.environnement.get_grille()[agent_pos[1]][agent_pos[0]]
+
+        while position is not None:
+            agent_pos = random.sample(self.every_possible_tuple_position, 1)
+            agent_pos = agent_pos[0]
+            position = self.environnement.get_grille()[agent_pos[1]][agent_pos[0]]
+
+        self.agent_list.append(Avatar("yellow", agent_pos[0], agent_pos[1],self.environnement, self.is_trace))
+
+    def init_walls(self, type="random"):
+        if type == "random":
+            n_walls = 100
+            agent_pos = random.sample(self.every_possible_tuple_position, n_walls)
+
+            for pos in agent_pos:
+                wall = Wall("brown", pos[0], pos[1], self.environnement)
+                self.agent_list.append(wall)
+
+
+        elif type == "labyrinth":
+            pass
+
+
     def create_new_defender(self):
         agent_pos = random.sample(self.every_possible_tuple_position, 1)
         agent_pos = agent_pos[0]
-        defender = Defender("green", agent_pos[0], agent_pos[1], self.environnement, self.is_trace, 500)
+        position = self.environnement.get_grille()[agent_pos[1]][agent_pos[0]]
+
+        while position is not None :
+            agent_pos = random.sample(self.every_possible_tuple_position, 1)
+            agent_pos = agent_pos[0]
+            position = self.environnement.get_grille()[agent_pos[1]][agent_pos[0]]
+
+        defender = Defender("green", agent_pos[0], agent_pos[1], self.environnement, self.is_trace, 100)
         self.environnement.set_agent(defender)
         self.agent_list.append(defender)
-        self.next_defender = 500
+        self.next_defender = 100
 
 
 
